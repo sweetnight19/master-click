2d0b8953d0cac3ab27c9ee8bb8d00230fc314930
David Marquet <david.marquet@students.salle.url.edu>
2021-04-29 20:59:45 +0200

    Añadir un botón o mecanismo para reiniciar el contador && Mostrar la media de clicks por usuario

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 Nuevas features a implementar:
 
 · (Cliente) Mostrar los clicks hechos por el usuario.
-· Mostrar los clicks totales.
+· (Servidor) Mostrar los clicks totales.
 · Mostrar la media de clicks por usuario y si el usuario está por encima y por debajo de la media.
-· Mostrar la cantidad de usuarios conectados.
-· Añadir un botón o mecanismo para reiniciar el contador.
+· (Servidor y cliente) Mostrar la cantidad de usuarios conectados.
+· (Cliente) Añadir un botón o mecanismo para reiniciar el contador.
 
 De todas las features, detectad qué se puede hacer en el cliente y qué se puede hacer en el servidor, implementadlo e indicádmelo en el README.md.
 
-Haced el deploy en heroku tal y como hemos visto en clase y en el README.md indicadme la URL.  Asimismo, si queréis indicarme la URL del repositorio del código en GitHub.
+URL Heroku:  
+URL del repositorio del código en GitHub:   https://github.com/sweetnight19/master-click.git
 
 Bonus:
 
--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 let text = document.querySelector("#chat");
 let sendButton = document.querySelector("#sendButton");
+let resetButton = document.querySelector("#resetButton");
 let textMessage = document.querySelector("#textMessage");
 
 let usersConnected = document.getElementById("counter");
@@ -7,6 +8,9 @@ let numClicksText = document.getElementById("clicksTxt");
 
 let ClicksUserText = document.getElementById("totalClicksUser");
 let ClicksUser = 0;
+let totalUsers = document.getElementById("totalUsers");
+let flag = 0;
+let averageclickText = document.getElementById("averageClicksUser");
 
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -28,7 +32,8 @@ socket.on("usuario conectado", (data) => {
     d.appendChild(t);
     text.appendChild(d);
 
-    usersConnected.innerText = data.usersConnected;
+    usersConnected.innerText //= data.usersConnected;//
+    totalUsers.innerText = data.usersConnected;
 });
 
 socket.on("usuario desconectado", (data) => {
@@ -40,24 +45,38 @@ socket.on("usuario desconectado", (data) => {
     d.appendChild(t);
     text.appendChild(d);
 
-    usersConnected.innerText = data.usersConnected;
+    usersConnected.innerText;
+    totalUsers.innerText = data.usersConnected;
 });
 
 socket.on("connect", () => {
     socket.emit("iam", urlParams.get("user"));
+    //totalUsers.innerText = data.usersConnected;
 });
 
 socket.on("numero de usuarios", (data) => {
     usersConnected.innerText = data.usersConnected;
+    totalUsers.innerText = data.usersConnected;
     numClicksText.innerHTML = data.numClicks;
     ClicksUserText.innerHTML = ClicksUser;
+
+    let average = ClicksUser * 100 / data.numClicks;
+    averageclickText.innerHTML = average.toFixed(2);
 });
 socket.on("new click", (data) => {
     numClicksText.innerText = data.numClicks;
     ClicksUser++;
     ClicksUserText.innerText = ClicksUser;
+    
+    let average = ClicksUser * 100 / data.numClicks;
+    averageclickText.innerHTML = average.toFixed(2);
 });
 
 sendButton.onclick = () => {
     socket.emit("click", "");
+};
+
+resetButton.onclick = () => {
+    ClicksUser = 0;
+    ClicksUserText.innerText = ClicksUser;
 };
